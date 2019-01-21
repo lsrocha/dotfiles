d8f21e9543ccc711cc99413f46ace64a1901fa49
Leonardo Rocha <leonardosrocha@outlook.com>
2019-01-21 17:50:48 -0200

    Removing dot from content name

--- prepare_env.sh
+++ prepare_env.sh
@@ -4,28 +4,28 @@ CURRENT_VAR=$(pwd)
 
 if [[ ! -e $HOME/.gitignore_global ]]; then
     echo 'CREATING GIT IGNORE'
-    ln -s $CURRENT_VAR/.gitignore_global $HOME/.gitignore_global
+    ln -s $CURRENT_VAR/gitignore_global $HOME/.gitignore_global
 else
     echo 'GIT IGNORE ALREADY EXISTS'
 fi
 
 if [[ ! -e $HOME/.bash_aliases ]]; then
     echo 'CREATING BASH ALIASES'
-    ln -s $CURRENT_VAR/.bash_aliases $HOME/.bash_aliases
+    ln -s $CURRENT_VAR/bash_aliases $HOME/.bash_aliases
 else
     echo 'BASH ALIASES ALREADY DEFINED'
 fi
 
 if [[ ! -d $HOME/.vim ]]; then
     echo 'LOADING VIM PLUGINS'
-    ln -s $CURRENT_VAR/.vim $HOME/.vim
+    ln -s $CURRENT_VAR/vim $HOME/.vim
 else
     echo 'VIM DIRECTORY ALREADY EXISTS'
 fi
 
 if [[ ! -e $HOME/.vimrc ]]; then
     echo 'LOADING VIM SETTINGS'
-    ln -s $CURRENT_VAR/.vimrc $HOME/.vimrc
+    ln -s $CURRENT_VAR/vimrc $HOME/.vimrc
 else
     echo 'VIMRC ALREADY EXISTS'
 fi
