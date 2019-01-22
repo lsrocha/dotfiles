afe2c66dead5acdca6e398a3567dacc5a357e5d0
Leonardo Rocha <leonardosrocha@outlook.com>
2019-01-22 13:15:57 -0200

    Refactoring directory tree

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,42 +1,33 @@
 #!/bin/bash
 
-CURRENT_DIR=$(pwd)
+SRC_DIR=$(pwd)/config
+CONFIG_DIR=$HOME/.config
 
-if [[ ! -e $HOME/.gitignore_global ]]; then
-    echo 'CREATING GIT IGNORE'
-    ln -s $CURRENT_DIR/gitignore_global $HOME/.gitignore_global
-else
-    echo 'GIT IGNORE ALREADY EXISTS'
-fi
+echo 'LOADING GIT SETTINGS'
+git config --global core.fileMode false
+git config --global core.excludesfile $SRC_DIR/git/gitignore_global
 
 if [[ ! -e $HOME/.bash_aliases ]]; then
     echo 'CREATING BASH ALIASES'
-    ln -s $CURRENT_DIR/bash_aliases $HOME/.bash_aliases
+    ln -s $SRC_DIR/bash/aliases $HOME/.bash_aliases
 else
     echo 'BASH ALIASES ALREADY DEFINED'
 fi
 
 if [[ ! -d $HOME/.vim ]]; then
-    echo 'LOADING VIM PLUGINS'
-    ln -s $CURRENT_DIR/vim $HOME/.vim
+    echo 'LOADING VIM SETTINGS'
+    ln -s $SRC_DIR/vim $HOME/.vim
 else
     echo 'VIM DIRECTORY ALREADY EXISTS'
 fi
 
-if [[ ! -e $HOME/.vimrc ]]; then
-    echo 'LOADING VIM SETTINGS'
-    ln -s $CURRENT_DIR/vimrc $HOME/.vimrc
-else
-    echo 'VIMRC ALREADY EXISTS'
+if [[ ! -d $CONFIG_DIR ]]; then
+     mkdir $CONFIG_DIR
 fi
 
-if [[ ! -d $HOME/.config ]]; then
-    mkdir $HOME/.config
-fi
-
-if [[ ! -d $HOME/.config/terminator ]]; then
+if [[ ! -d $CONFIG_DIR/terminator ]]; then
     echo 'LOADING TERMINATOR SETTINGS'
-    ln -s $CURRENT_DIR/config/terminator $HOME/.config/terminator
+    ln -s $SRC_DIR/terminator $CONFIG_DIR/terminator
 else
     echo 'TERMINATOR CONFIG ALREADY EXISTS'
 fi
